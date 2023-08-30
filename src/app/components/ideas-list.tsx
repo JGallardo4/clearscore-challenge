8f3657ab8404a82c7296edfc51ed88bc3d0d1e29
Juan Gallardo <juan.gallardo.ab@gmail.com>
2023-08-29 21:08:47 -0600

    Remove unused code

--- src/app/components/ideas-list.tsx
+++ src/app/components/ideas-list.tsx
@@ -22,22 +22,6 @@ export default function IdeasList() {
     setIdeas(ideas.filter((idea) => idea.id != ideaId))
   }
 
-  const MAX_CHARACTERS_DESCRIPTION = 140;
-  const ideaSchema = Yup.object().shape({
-    title: Yup.string()
-      .min(2, 'Too Short!')
-      .max(20, 'Too Long!')
-      .required('Required'),
-    description: Yup.string()
-      .min(2, 'Too Short!')
-      .max(
-        MAX_CHARACTERS_DESCRIPTION,
-        `Please enter a maximum of ${MAX_CHARACTERS_DESCRIPTION} characters`
-      )
-      .required('Required'),
-    lastUpdated: Yup.date(),
-  });
-
   return !ideas ? (
     <p>No items found</p>
   ) : (
